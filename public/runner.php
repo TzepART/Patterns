3924e61fd7f77d4c63c3fc88c93499fcf7811b13
Artem Tsepkov <tzepart@gmail.com>
2020-06-01 02:15:18 +0300

    fix

--- public/runner.php
+++ public/runner.php
@@ -2,19 +2,19 @@
 <?php
 declare(strict_types=1);
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$patternType = $argv[1];
-$patternName = $argv[2];
+$patternType = $argv[1] ?? '';
+$patternName = $argv[2] ?? '';
 
 use Patterns\BehavioralPatterns;
 use Patterns\CreationalPatterns;
 use Patterns\StructuralPatterns;
 use \Patterns\RunnerInterface;
 
-$runnerName = sprintf('Patterns\\%s\\%s\\Runner',$patternType,$patternName);
+$runnerName = sprintf('Patterns\\%s\\%s\\Runner', $patternType, $patternName);
 
 /** @var RunnerInterface $runnerName */
-if(class_exists($runnerName)){
+if (class_exists($runnerName)) {
     $runnerName::using();
 }
